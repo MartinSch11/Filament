8f87d21b6d32472f812eff76e85515d84976d831
Emanuel Prestes <prestemanuel@gmail.com>
2025-01-14 19:35:32 -0300

    Mas cambios

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,35 +3,86 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\City;
+use App\Models\State;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
+    protected static ?string $navigationLabel = 'Employees';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required(),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required(),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required(),
+                Section::make('Información de la Persona')
+                    ->columns(3)
+                    ->schema([
+                        Forms\Components\TextInput::make('Nombre')
+                            ->required(),
+                        Forms\Components\TextInput::make('Email')
+                            ->email()
+                            ->required(),
+                        Forms\Components\TextInput::make('Contraseña')
+                            ->password()
+                            ->required(),
+                    ]),
+
+                Section::make('Dirección de la Persona')
+                    ->columns(3)
+                    ->schema([
+                        Forms\Components\Select::make('country_id')
+                            ->relationship(name: 'country', titleAttribute: 'name')
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->afterStateUpdated(function (Set $set) {
+                                $set('state_id', null);
+                                $set('city_id', null);
+                            })
+                            ->required(),
+
+                        Forms\Components\Select::make('state_id')
+                            ->options(
+                                fn(Forms\Get $get): Collection => State::query()
+                                    ->where('country_id', $get('country_id'))
+                                    ->pluck('name', 'id')
+                            )
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->afterStateUpdated(fn(Forms\Set $set) => $set('city_id', null))
+                            ->required(),
+
+                        Forms\Components\Select::make('city_id')
+                            ->options(
+                                fn(Forms\Get $get): Collection => City::query()
+                                    ->where('state_id', $get('state_id'))
+                                    ->pluck('name', 'id')
+                            )
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->required(),
+
+                            Forms\Components\TextInput::make('Dirección')
+                            ->required(),
+
+                            Forms\Components\TextInput::make('Código Postal')
+                            ->required(),
+                    ]),
             ]);
     }
 
